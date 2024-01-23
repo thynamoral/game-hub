85b1978104efca1bd95e5f30fa052228f91395c7
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-01-23 22:40:28 +0700

    Implement dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "components/Navbar";
 
 function App() {
   return (
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../../assets/game-hub-logo.png";
+import logo from "assets/game-hub-logo.png";
 
 const Navbar = () => {
   return (
