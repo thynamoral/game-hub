1f25b435d7bf61db7af0e721cb54cc7fb353e6e5
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-01-24 15:09:34 +0700

    make a custom hook to fetch the games

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import apiClient, { AxiosError, CanceledError } from "services/api-client";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        if (error instanceof AxiosError) setError(error.message);
+        console.log(error.message);
+        setIsLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
