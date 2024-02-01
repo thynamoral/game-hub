21bf2763d3868d3a2f234c1966dafe7e76e9e73a
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-02-01 22:41:08 +0700

    optimized images

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,16 +1,29 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  useColorMode,
+} from "@chakra-ui/react";
 import { CriticScore } from "components/CriticScore";
 import { PlatformIconList } from "components/PlatformIconList";
 import { Game } from "hooks/useGames";
+import croppedImageUrl from "services/img-url";
 
 interface GameCardProps {
   game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
+  const { colorMode } = useColorMode();
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card
+      borderRadius={10}
+      overflow="hidden"
+      bg={colorMode === "light" ? "gray.400" : ""}
+    >
+      <Image src={croppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading size="md">{game.name}</Heading>
         <HStack justifyContent="space-between">
