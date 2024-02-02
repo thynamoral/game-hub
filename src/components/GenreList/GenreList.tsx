b8bd646c6ab5e18e04d5d22fb598dd0e6e0b355a
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-02-02 23:57:48 +0700

    display GenreSkeleton

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,10 +1,18 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { GenreSkeleton } from "components/GenreSkeleton";
 import useGenres from "hooks/useGenres";
 import croppedImageUrl from "services/img-url";
 
 const GenreList = () => {
-  const { data: genres } = useGenres();
-  return (
+  const { data: genres, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  return isLoading ? (
+    <List spacing={"10px"}>
+      {skeletons.map((skeleton) => {
+        return <GenreSkeleton key={skeleton} />;
+      })}
+    </List>
+  ) : (
     <List>
       {genres.map((genre) => (
         <ListItem key={genre.id} paddingY={"5px"}>
---
+++ src/components/GenreSkeleton/GenreSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@chakra-ui/react";
+
+const GenreSkeleton = () => {
+  return <Skeleton height="40px" borderRadius="5px" />;
+};
+
+export default GenreSkeleton;
