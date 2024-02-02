c718ef7edfd07cf0687ea0577853a5f0060f31ea
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-02-02 23:02:58 +0700

    refactor generic fecting data hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,42 +1,13 @@
-import { useEffect, useState } from "react";
-import apiClient, { AxiosError, CanceledError } from "services/api-client";
+import useData from "./useData";
 
-interface Genre {
-  id: string;
+export interface Genre {
+  id: number;
   name: string;
 }
 
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
 const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-
-    apiClient
-      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        if (error instanceof AxiosError) setError(error.message);
-        console.log(error.message);
-        setIsLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
+  const { data } = useData<Genre>("/genres");
+  return { data };
 };
 
 export default useGenres;
