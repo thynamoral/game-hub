4d5af3e12caadde57d25e2d6b24d1f0c9ee9ff0f
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-02-08 11:20:18 +0700

    implement searching the games

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "assets/game-hub-logo.png";
 import { ColorModeSwitch } from "components/ColorModeSwitch";
+import { SearchInput } from "components/SearchInput";
 
-const Navbar = () => {
+interface Props {
+  onSearchGame: (searchQuery: string) => void;
+}
+
+const Navbar = ({ onSearchGame }: Props) => {
   return (
-    <HStack justifyContent="space-between" px={5}>
+    <HStack gap={"20px"} px={5}>
       <Image src={logo} boxSize="60px" width="120px" objectFit="contain" />
+      <SearchInput onSearchGame={onSearchGame} />
       <ColorModeSwitch />
     </HStack>
   );
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,43 @@
+import { SearchIcon } from "@chakra-ui/icons";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+
+interface Props {
+  onSearchGame: (searchQuery: string) => void;
+}
+
+const SearchInput = ({ onSearchGame }: Props) => {
+  const [query, setQuery] = useState("");
+  const searchRef = useRef<HTMLInputElement>(null);
+  const handleSearchGames = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setQuery(value);
+  };
+  // handle submit
+  const handleSearchSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (searchRef.current) {
+      const { value } = searchRef.current;
+      onSearchGame(value);
+    }
+  };
+  return (
+    <form onSubmit={handleSearchSubmit}>
+      <InputGroup>
+        <InputLeftElement>
+          <SearchIcon color="gray.300" />
+        </InputLeftElement>
+        <Input
+          placeholder="Search the games"
+          borderRadius="2rem"
+          variant="filled"
+          value={query}
+          onChange={handleSearchGames}
+          ref={searchRef}
+        />
+      </InputGroup>
+    </form>
+  );
+};
+
+export default SearchInput;
