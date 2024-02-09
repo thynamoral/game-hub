a7afb56c93c6038b9545a8b5476ed5ce36f44620
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-02-09 13:24:05 +0700

    cleaning up the GameCard component

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,4 +1,10 @@
-import { CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import {
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  useColorMode,
+} from "@chakra-ui/react";
 import { CriticScore } from "components/CriticScore";
 import { GameCardContainer } from "components/GameCardContainer";
 import { PlatformIconList } from "components/PlatformIconList";
@@ -10,18 +16,20 @@ interface GameCardProps {
 }
 
 const GameCard = ({ game }: GameCardProps) => {
-  // const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   return (
     <GameCardContainer>
       <Image src={croppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading size="md">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+      <CardBody bg={colorMode === "light" ? "gray.800" : ""}>
+        <HStack justifyContent="space-between" marginBottom="10px">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore metacritic={game.metacritic} />
         </HStack>
+        <Heading color={colorMode === "light" ? "white" : ""} size="md">
+          {game.name}
+        </Heading>
       </CardBody>
     </GameCardContainer>
   );
