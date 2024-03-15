9a88ffd3d8f4301ab49d9f781f4d4e21272c6e34
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-03-15 16:28:19 +0700

    Implement infiniteQuery in react-query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { gameQuery } from "App";
 import APIClient from "services/api-client";
 import { Genre } from "./useGenres";
@@ -16,18 +16,22 @@ export interface Game {
 const apiClient = new APIClient<Game>("/games");
 
 const useGames = (gameQuery: gameQuery) => {
-  return useQuery({
+  return useInfiniteQuery({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery?.sortOrder,
           search: gameQuery.searchQuery,
+          page: pageParam,
         },
       }),
     staleTime: 24 * 60 * 60 * 1000, // 24h
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   });
 };
 
