5857b8044288b56be7ada37b769b68345ad234d7
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-03-22 15:24:38 +0700

    fixing the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, HStack, Image } from "@chakra-ui/react";
 import logo from "assets/game-hub-logo.png";
 import { ColorModeSwitch } from "components/ColorModeSwitch";
 import { SearchInput } from "components/SearchInput";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -11,7 +12,9 @@ const Navbar = () => {
       flexDirection={{ base: "column", md: "row" }}
       alignItems={{ base: "flex-start", md: "center" }}
     >
-      <Image src={logo} boxSize="60px" width="120px" objectFit="contain" />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" width="120px" objectFit="contain" />
+      </Link>
       <Flex gap="10px" w="100%">
         <Box flexGrow={1}>
           <SearchInput />
