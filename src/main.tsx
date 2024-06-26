87e18333732cfb4db7eabbc0434aedd13bd38ed9
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-06-26 23:08:32 +0700

    move routing-file to a folder

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
-import router from "routes";
+import router from "./routes";
 import theme from "theme.ts";
 import "./index.css";
 
