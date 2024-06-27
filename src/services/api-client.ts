59808334bf7e93a30a410bfa5d29af911a4d2726
Namoral Thy <99861498+thynamoral@users.noreply.github.com>
2024-06-27 14:34:18 +0700

    Revert "hide api-key"
    This reverts commit 41dc9696e1773ec5972267421504fa673ac7dbc9.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_API_KEY,
+    key: "a240b3088b574ef4bf392b20d1f98fdd",
   },
   withCredentials: false,
 });
